847089caf6a9d14daba20d89af8dedfd2f9db94a
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-03-15 10:05:58 +0200

    Update get_birthday.py
    added testing data

--- get_birthday.py
+++ get_birthday.py
@@ -27,3 +27,15 @@ def get_birthdays_per_week(users):
         for name in this_week_b_days[day]:
             message += ' ' + name +','
         print(message[:len(message)-1])#printing without last coma.
+
+USERS = [{"name": "Jonatan", "birthday": datetime.date(2000, 10, 24)},
+         {"name": "Clarkson", "birthday": datetime.date(1980, 1, 1)},
+         #can be used to test birthdays this week but next year
+         {"name": "Prower", "birthday": datetime.date(1992, 11, 24)},
+         {"name": "Samuel", "birthday": datetime.date(1987, 3, 18)},
+         {"name": "Wiliam", "birthday": datetime.date(1983, 3, 18)},
+         #multiple birthdays one day
+         {"name": "Gibson", "birthday": datetime.date(1980, 3, 17)}]
+         #earlier birthday is later in the list. It must be displayed first
+
+get_birthdays_per_week(USERS)
